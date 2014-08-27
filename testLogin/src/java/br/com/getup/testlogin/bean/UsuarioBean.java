7b89f7ad907afe46bca179b33a387d5116f06710
MarkusPatriota <MarkusPatriota@ASUS-PC>
2014-08-27 20:15:25 -0300

    Criação da estrutura do projeto

---
+++ testLogin/src/java/br/com/getup/testlogin/bean/UsuarioBean.java
@@ -0,0 +1,9 @@
+package br.com.getup.testlogin.bean;
+
+/**
+ *
+ * @author MarkusPatriota
+ */
+public class UsuarioBean {
+    
+}
---
+++ testLogin/src/java/br/com/getup/testlogin/model/Usuario.java
@@ -0,0 +1,14 @@
+package br.com.getup.testlogin.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author MarkusPatriota
+ */
+@Entity
+@Table(name="usuario_tb")
+public class Usuario {
+    
+}
